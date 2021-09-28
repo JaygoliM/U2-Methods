cdf43683fcede62a0878594e4f5ed185af38ac68
JaygoliM <golidjay000@warren.k12.in.us>
2021-09-28 13:07:36 -0400

    FEET TO MTERJPKE

--- L5-Swing Frames and Buttons/src/FeetToMeters.java
+++ L5-Swing Frames and Buttons/src/FeetToMeters.java
@@ -1,6 +1,49 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
 public class FeetToMeters {
 
     /* Create a program with two buttons. Clicking on the first button will prompt the user to enter a distance in feet and
     convert it to meters. Clicking on the second button will prompt the user to enter a distance in meters and convert to feet.
-     */
+    */
+    public static void main(String[] args) {
+        JFrame window = new JFrame("FeetToMeters");
+        JPanel panel = new JPanel();
+        JButton FTM = new JButton("Feet to Meters");
+        JButton MTF = new JButton("Meters to Feet");
+        window.setSize(350, 200);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+
+        FTM.addActionListener(new FeetToMeter());
+        MTF.addActionListener(new MeterToFeet());
+
+        panel.add(MTF);
+        panel.add(FTM);
+        window.add(panel);
+        window.setVisible(true);
+    }
+
+    private static class FeetToMeter implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            double feet = Double.parseDouble(JOptionPane.showInputDialog("Enter Feet"));
+            double meters = feet / 3.28;
+
+            JOptionPane.showMessageDialog(null, meters + " meters.");
+        }
+    }
+        private static class MeterToFeet implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
+                double meters = Double.parseDouble(JOptionPane.showInputDialog("Enter Meters"));
+                double feet = meters * 3.28;
+
+                JOptionPane.showMessageDialog(null, feet + " feet.");
+            }
+        }
+
 }
+
+
--- L5-Swing Frames and Buttons/src/Quotes.java
+++ L5-Swing Frames and Buttons/src/Quotes.java
@@ -1,8 +1,44 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Quotes {
 
     /*Create a GUI program with three buttons.
     Each button displays one of your favorite quotes.
     Hint: Use \n to make a new line.
      */
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        JPanel panel = new JPanel();
+        JButton button1 = new JButton("I'm Quote 1");
+        JButton button2 = new JButton("I'm Quote 2");
+        JButton button3 = new JButton("I'm Quote 3");
 
+        window.setSize(350,100);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+
+        button1.addActionListener(new ButtonListener1());
+        button2.addActionListener(new ButtonListener2());
+        button3.addActionListener(new ButtonListener3());
+
+
+        panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
+        window.add(panel);
+        window.setVisible(true);
+    }
+    private static class ButtonListener1 implements ActionListener {
+        public void actionPerformed(ActionEvent e) {JOptionPane.showMessageDialog(null,"You will survive 100 years if you wish to!!!! :D");}
+    }
+
+    private static class ButtonListener2 implements ActionListener {
+        public void actionPerformed(ActionEvent e) {JOptionPane.showMessageDialog(null,"Bad things are around the corner :((");}
+    }
+    private static class ButtonListener3 implements ActionListener {
+        public void actionPerformed(ActionEvent e) {JOptionPane.showMessageDialog(null,"Your luck is looking up! >:)))");}
+    }
 }
