e41dae3ff86323121b9d2b7841ba1b66da99c2a9
JaygoliM <golidjay000@warren.k12.in.us>
2021-10-22 12:39:50 -0400

    I DID MYSELF

--- L7-Swing Colors and Paint/src/ChangingColorsGUI.java
+++ L7-Swing Colors and Paint/src/ChangingColorsGUI.java
@@ -1,44 +1,61 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ChangingColorsGUI {
 
-    //Variables in a class are declared here,
-    //but often not initialized
+    public class ChangingColorsGUI  {
 
-    JFrame window;
-    JPanel panel;
-    JButton colorClicker;
+        //Variables in a class are declared here,
+        //but often not initialized
 
-    /*
-    This is a constructor and absolutely necessary for a class.
-    This is where we will build the GUI, it must have the same
-    name as the class.
-    */
+        JFrame window;
+        JPanel panel;
+        JButton colorClicker;
+        Color panelBack = new Color(14, 45, 175);
+        Color buttonBack = new Color(255, 255, 255);
+        Color buttonFront = new Color(103, 100, 100, 255);
+        /*
+        This is a constructor and absolutely necessary for a class.
+        This is where we will build the GUI, it must have the same
+        name as the class.
+        */
 
-    public ChangingColorsGUI(){
+        public ChangingColorsGUI() {
 
-        window = new JFrame("Color Changer");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(340, 127);
-        window.setLocationRelativeTo(null);
 
-        panel = new JPanel();
-        colorClicker = new JButton("Change the Colors");
+            window = new JFrame("Color Changer");
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.setSize(340, 127);
+            window.setLocationRelativeTo(null);
+///
+            panel = new JPanel();
+            colorClicker = new JButton("Change the Colors");
 
-        //create a new Custom color
+            //create a new Custom color
 
-        //Set the colors of our buttons and panel
 
-        panel.add(colorClicker);
-        window.add(panel);
+            //Set the colors of our buttons and panel
+            panel.setBackground(panelBack);
+            colorClicker.setForeground(buttonBack);
+            colorClicker.setBackground(buttonFront);
 
-        window.setVisible(true);
+            colorClicker.addActionListener(new ColorClickerListener());
+            panel.add(colorClicker);
+            window.add(panel);
+
+            window.setVisible(true);
+        }
+
+        //Add a listener to change the color when the button is clicked
+        private class ColorClickerListener implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                panel.setBackground(buttonBack);
+                colorClicker.setForeground(panelBack);
+                colorClicker.setBackground(buttonFront);
+
+            }
+        }
     }
 
-    //Add a listener to change the color when the button is clicked
-
-
-
-
-}
--- L7-Swing Colors and Paint/src/ColorSwitchGUI.java
+++ L7-Swing Colors and Paint/src/ColorSwitchGUI.java
@@ -1,11 +1,72 @@
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ColorSwitchGUI {
 
     //Declare your frame, panel and three buttons.
+
+    JFrame window;
+    JPanel panel;
+
+    JButton colorBlue;
+    JButton colorRed;
+    JButton colorYellow;
+   Color coolBlue = new Color(176,224,230);
+   Color Blueback = new Color(72, 72, 253);
+   Color Bluefront = new Color(5, 30, 132);
+   Color NiceRed = new Color(128,0,0);
+    Color Redback = new Color(255, 0, 0);
+    Color Redfront = new Color(255, 2, 2);
+   Color WeirdYellow = new Color(255, 229, 70);
+    Color Yellowback = new Color(255, 211, 67);
+    Color Yellowfront = new Color(255, 242, 154);
+
     public ColorSwitchGUI(){
         //create your GUI
+        window = new JFrame("Color Switcher");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(340, 127);
+        window.setLocationRelativeTo(null);
+///
+        panel = new JPanel();
+        colorBlue = new JButton("Change the color blue");
+        colorRed = new JButton("Change the color red");
+        colorYellow = new JButton("Change the color yellow");
+
+        colorBlue.addActionListener(new BlueColorListener());
+        colorRed.addActionListener(new RedColorListener());
+        colorYellow.addActionListener(new YellowColorListener());
+        panel.add(colorBlue);
+        panel.add(colorRed);
+        panel.add(colorYellow);
+
+        window.add(panel);
+
+        window.setVisible(true);
     }
 
     //add listeners to change the colors. Don't forget to link them to buttons.
+    private class BlueColorListener implements ActionListener {
+        public void actionPerformed(ActionEvent actionEvent) {
+            panel.setBackground(coolBlue);
+            colorBlue.setForeground(Bluefront);
+            colorBlue.setBackground(Blueback);
+        }
+    }
+    private class RedColorListener implements ActionListener {
+        public void actionPerformed(ActionEvent actionEvent) {
+            panel.setBackground(NiceRed);
+            colorRed.setForeground(Redfront);
+            colorRed.setBackground(Redback);
+        }
+    }
+    private class YellowColorListener implements ActionListener {
+        public void actionPerformed(ActionEvent actionEvent) {
+            panel.setBackground(WeirdYellow);
+            colorYellow.setForeground(Yellowfront);
+            colorYellow.setBackground(Yellowback);
+        }
+    }
 }
